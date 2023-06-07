b7c147cfaa044944e6f64b617151fee573d267d1
MylesMburu <ngicumyles@gmail.com>
2023-06-07 19:17:54 +0300

    genre api worked

---
+++ amany/src/compenents/Dropdown.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+
+const Dropdown = props => {
+  const [selectedValue, setSelectedValue] = useState('');
+    
+  return (
+    <div>
+        <select value={selectedValue} onChange={e => setSelectedValue(e.target.value)}>
+            {props.options.map((item,idx) => <option key={idx} value={item.value}>{item.name}</option>)}
+        </select>
+        <p>You chose {selectedValue}</p>
+    </div>
+  )
+}
+
+export default Dropdown
--- amany/src/compenents/Mood.jsx
+++ amany/src/compenents/Mood.jsx
@@ -1,23 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import IMG1 from "../assets/harp_bitmoji.png";
+import Dropdown from './Dropdown';
+import axios from 'axios';
 
 export const Mood = () => {
-  const CLIENT_ID = "3186fea7250343ccb3e3862db742cd89";
-  const SECRET_KEY = import.meta.env.VITE_SECRET_KEY;
-  const REDIRECT_URI = "http://localhost:3000/";
-  const AUTH_ENDPOINT = "https://account.spotify.com/authorize";
-  const RESPONSE_TYPE = "code";
+  const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
+  const SECRET_KEY = import.meta.env.VITE_CLIENT_SECRET;
+
+  const data =[
+    {value:1, name:'A'},
+    {value:2, name:'B'},
+    {value:3, name:'C'},
+]
+
+const [token, setToken] = useState('');
+const [genres, setGenres] = useState([]);
+
+useEffect(()=>{
+  axios('https://accounts.spotify.com/api/token', {
+    headers: {
+      'Content-Type' : 'application/x-www-form-urlencoded',
+      'Authorization' : 'Basic ' + btoa(CLIENT_ID + ':' + SECRET_KEY)      
+    },
+    data: 'grant_type=client_credentials',
+    method: 'POST'
+  })
+  .then(tokenResponse => {      
+    console.log(tokenResponse.data.access_token)
+    setToken(tokenResponse.data.access_token);
+
+    axios('https://api.spotify.com/v1/browse/categories?locale=sv_US', {
+      method: 'GET',
+      headers: { 'Authorization' : 'Bearer ' + tokenResponse.data.access_token}
+    })
+    .then(genreResponse => {
+      setGenres(genreResponse.data.categories.items);
+    });
+  });
+
+
+
+    
+}, []);
 
   return (
     <div className='grid grid-cols-2' id='Mood'>
       <div className='bg-red-600 h-full'>
         <div>
-        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>
-          Log into Spotify
-        </a>
-        </div>
-        <div>
-
+        <form action="" onSubmit={()=>{}}>
+        <Dropdown options={genres}/>
+        <button type="submit">Get music</button>
+        </form>
         </div>
       </div>
 
