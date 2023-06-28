9315d1751b5d0b34ccc94ecaf3e7ba48c3696107
MylesMburu <ngicumyles@gmail.com>
2023-06-28 09:11:18 +0300

    sms button

--- amany/src/compenents/Wellness/SearchForm.jsx
+++ amany/src/compenents/Wellness/SearchForm.jsx
@@ -24,6 +24,16 @@ function SearchForm() {
     }
   };
 
+  const handleSendSMS = async () => {
+    try {
+      await axios.post('http://localhost:8000/send-sms');
+      alert('Message sent successfully');
+    } catch(err) {
+      console.error(err);
+      alert('Failed to send message');
+    }
+  };
+
   return (
     <div className='flex flex-col w-max text-black'>
       <input placeholder="County" value={county} onChange={e => setCounty(e.target.value)} className='input-box'/>
@@ -31,12 +41,13 @@ function SearchForm() {
       <input placeholder="Gender" value={gender} onChange={e => setGender(e.target.value)} className='input-box'/>
       <button onClick={handleSearch} className='py-2 px-4 bg-secondary text-white w-max rounded-md'>Search</button>
 
-      {results.length > 0 ? (    //checks if there is a result and renders it if found
+        {results.length > 0 ? (
         results.map((result, i) => (
-        <div key={i} className='bg-secondary text-white rounded-md m-8 px-6 py-2'>
-          <p>Name: {result.name}</p>
-          <p>Phone Number: {result.phoneNumber}</p>
-        </div>
+          <div key={i} className='bg-secondary text-white rounded-md m-8 px-6 py-2'>
+            <p>Name: {result.name}</p>
+            <p>Phone Number: {result.phoneNumber}</p>
+            <button onClick={handleSendSMS}>Send SMS</button>
+          </div>
         ))
         ):  //else, gives a message that there has been no result
         (
