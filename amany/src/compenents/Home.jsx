45db0d2198d6e4047750622d24ac39a9563218b5
Kibet Immanuel Ng'eno <kibetimmanuel0@gmail.com>
2023-06-29 16:10:56 +0300

    simulate-donate pay

--- amany/src/compenents/Home.jsx
+++ amany/src/compenents/Home.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import Landing from './Landing';
+import React, { useState } from 'react'
+import axios from 'axios'
+import Landing from './Landing'
 
 export const Home = () => {
   const [formData, setFormData] = useState({
@@ -9,65 +9,72 @@ export const Home = () => {
     county: '',
     ward: '',
     availability: '',
-    phoneNumber: ''
-  });
+    phoneNumber: '',
+  })
 
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
     axios
       .post('http://localhost:8000/register', formData)
       .then((response) => {
-        console.log(response.data); // Registration successful
+        console.log(response.data) // Registration successful
         setFormData({
           name: '',
           gender: '',
           county: '',
           ward: '',
           availability: '',
-          phoneNumber: ''
-        });
-        window.alert("Thank you, your registration was successful!")
+          phoneNumber: '',
+        })
+        window.alert('Thank you, your registration was successful!')
       })
       .catch((error) => {
-        console.error('Error:', error);
-      });
-  };
+        console.error('Error:', error)
+      })
+  }
 
   return (
     <div className='bg-primary text-white'>
-
       <Landing />
       <div className='px-8'>
         <div className=' px-auto flex justify-center'>
-        <h1 className='font-bold text-5xl py-8'>Did You Know?</h1>
-       
+          <h1 className='font-bold text-5xl py-8'>Did You Know?</h1>
         </div>
         <div className='grid grid-cols-3'>
           <div className='flex items-center justify-center bg-white text-black rounded-full w-80 h-80'>
             <p className='p-10'>
-              There are approximately <b>100 psychiatrists</b> for a population of <b>45 million in Kenya</b>. <br /> Roughly translates to a ratio of 1 psychiatrist for every 450,000 people
+              There are approximately <b>100 psychiatrists</b> for a population
+              of <b>45 million in Kenya</b>. <br /> Roughly translates to a
+              ratio of 1 psychiatrist for every 450,000 people
             </p>
           </div>
           <div className='flex items-center justify-center bg-white text-black rounded-full w-80 h-80'>
             <p className='p-12'>
-              The WHO's 2017 report on the world mental health situation ranked Kenya <b>5th among African countries</b> with the highest number of depression cases.
+              The WHO's 2017 report on the world mental health situation ranked
+              Kenya <b>5th among African countries</b> with the highest number
+              of depression cases.
             </p>
           </div>
           <div className='flex items-center justify-center bg-white text-black rounded-full w-80 h-80'>
             <p className='p-11'>
-              In Kenya, <b>1 in 4 people</b> seeking healthcare have a mental health condition, including high rates of depression and increasing substance/alcohol use disorders.
+              In Kenya, <b>1 in 4 people</b> seeking healthcare have a mental
+              health condition, including high rates of depression and
+              increasing substance/alcohol use disorders.
             </p>
           </div>
         </div>
       </div>
       <div className='flex flex-col justify-center items-center pt-8'>
         <h2>CHV Registration Form</h2>
-        <form className='flex flex-col p-8 w-full md:w-96 text-black' onSubmit={handleSubmit}>
+        <form
+          className='flex flex-col p-8 w-full md:w-96 text-black'
+          onSubmit={handleSubmit}
+        >
           <input
             type='text'
             name='name'
@@ -141,11 +148,14 @@ export const Home = () => {
             value={formData.phoneNumber}
             onChange={handleChange}
           />
-          <button type='submit' className='py-2 px-4 bg-secondary text-white w-max rounded-md'>
+          <button
+            type='submit'
+            className='py-2 px-4 bg-secondary text-white w-max rounded-md'
+          >
             Register
           </button>
         </form>
       </div>
     </div>
-  );
-};
+  )
+}
--- amany/src/compenents/Landing.jsx
+++ amany/src/compenents/Landing.jsx
@@ -1,27 +1,26 @@
-import React from 'react'
+import SimulatePaymentModal from './SimulatePaymentModal'
 
 export default function Landing() {
-
-
-  
   return (
-    <div className='flex items-center justify-center h-screen'>
-    <div className='w-1/2 p-8 relative'>
-      <h1 className='absolute text-5xl'>Nurturing Mental Resilience</h1>
-      <br />
-      <br />
-      <br />
-      <button className='text-3xl bg-white rounded-xl uppercase ml-60 px-4  py-2 hover:bg-gray-100 text-primary font-bold'>
-         <a href="#" classname=""> Donate ❤️</a>
-      </button>
-      
-    </div>
-    <div className='w-1/2'>
-      <img className='w-full' src="https://uploads-ssl.webflow.com/60fff0c2ee17fc46c9f078db/6102a5804733360b13bd1fd4_great-minds-introduction-working.svg" alt="" />
-    </div>
+    <>
+      <div className='flex items-center justify-center h-screen'>
+        <div className='w-1/2 p-8'>
+          <h1 className='absolute text-5xl'>Nurturing Mental Resilience</h1>
+          <br />
+          <br />
+          <br />
+          <SimulatePaymentModal />
+        </div>
 
-    
-  </div>
-  
+        <div className='w-1/2'>
+          <img
+            className='w-full'
+            src='https://uploads-ssl.webflow.com/60fff0c2ee17fc46c9f078db/6102a5804733360b13bd1fd4_great-minds-introduction-working.svg'
+            alt=''
+          />
+        </div>
+      </div>
+      {/* <div className='absolute top-0 left-0 w-full'></div> */}
+    </>
   )
 }
